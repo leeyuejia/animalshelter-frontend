d254cfe9bc62415dd8d135b800011ea230d56b28
YJ Lee <leeyuejia@gmail.com>
2020-07-23 20:56:56 +0800

    refined props in animalcard.js

--- src/component/AnimalCard.js
+++ src/component/AnimalCard.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function AnimalList(props) {
     return (
         <div>
-            <h1>{this.props.name}</h1>
-            <h2>{this.props.species}</h2>
+            <h1>{props.name}</h1>
+            <h2>{props.species}</h2>
         </div>
     )
 }
